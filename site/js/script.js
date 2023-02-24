e9a0adf9eb214df0d25d817247ebe28e67597aae
Eugene Kos <mstrsid@live.com>
2023-02-24 10:13:50 +0300

    feature done

--- site/js/script.js
+++ site/js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+    const headerMain = document.querySelector(".header__main");
+    const headerSub = document.querySelector(".header__sub");
     console.log('Hello');
+    console.log(headerMain);
+    console.log(headerSub);
 });
